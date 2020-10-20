412df6e423eaba57ffded4333e1ee6e018324a07
FirasSoekhai <54799485+FirasSoekhai@users.noreply.github.com>
2020-10-20 18:44:10 +0200

    Add files via upload

---
+++ Vragen.md
@@ -0,0 +1,16 @@
+### Mijn vragen over de code in deze opdracht
+Vraag 1: Wat doet dat @return "iets" in de comments bij PHP???
+
+#### Antwoorden van klasgenoot: 
+
+#### Vragen die we samen hebben
+
+#### Antwoorden van de klas
+
+---
+
+### Vragen en opmerkingen bij het maken van deze opdracht
+- Waar kom je niet uit? 
+- Wat is nog niet duidelijk?
+- Wat heb je geprobeerd?
+- Waar loop je tegenaan?
---
+++ classes/ShoppingCart.php
@@ -0,0 +1,62 @@
+<?php
+
+class ShoppingCart
+{
+
+	/**
+	 * Stores all the products in the Shopping Cart
+	 * @var array
+	 */
+    private $products;
+
+    public function __construct()
+    {
+        $this->products = [];
+    }
+
+    /**
+     * Add a product
+     * @param $product
+     */
+    public function addProduct($product)
+    {
+        $this->products[] = $product;
+    }
+
+    /**
+     * Remove an item from the shopping cart at a position
+     * @param $index
+     */
+    public function removeItem($position)
+    {
+        array_splice($this->products, $position, 1);
+    }
+
+    /**
+     * Clear's the product array.
+     */
+    public function clearCart()
+    {
+        $this->products = [];
+    }
+
+    /**
+     * Returns all products in the shopping cart.
+     * @return array
+     */
+    public function getProducts()
+    {
+        return $this->products;
+    }
+
+	/**
+	 * Check if the sopping cart has products
+	 *
+	 * @return bool
+	 */
+    public function hasProducts()
+    {
+        return !empty($this->products);
+    }
+
+}
